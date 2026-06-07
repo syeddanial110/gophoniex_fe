5f7fd651f2a89ef94738687ce8ea9bfe9ab391f3
Syed Danial <daniald3200@gmail.com>
2026-06-07 23:25:51 +0500

    env setup done tested

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -1,8 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-// export const stripePromise = loadStripe(
-//   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-// );
 export const stripePromise = loadStripe(
-  "pk_test_51SIGBNQlUNdtR5hzNaqbpJd8W3ACLTS2JjW3omxpRmgLSOoczOY4AnAFkwPYODv9Won0ZdycWTfGi2PTesusxxGO00UcXyLMnA"
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
 );
